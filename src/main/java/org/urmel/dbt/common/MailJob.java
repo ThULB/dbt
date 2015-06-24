b2491888cc72e980f3e9ba110a94cd24adbf560c
René Adler <adlerre@users.noreply.github.com>
2015-06-24 14:04:58 +0000

    implement mailing queue, resolver based
    git-svn-id: https://svn.thulb.uni-jena.de/repos/dbt/trunk@2391 f4d8827f-d2b3-4e18-a982-0b0116c0c996

---
+++ src/main/java/org/urmel/dbt/common/MailJob.java
@@ -0,0 +1,91 @@
+/*
+ * $Id$ 
+ * $Revision$ $Date$
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+package org.urmel.dbt.common;
+
+import java.util.concurrent.ExecutionException;
+
+import org.jdom2.Element;
+import org.mycore.common.MCRMailer;
+import org.mycore.common.xml.MCRURIResolver;
+import org.mycore.services.queuedjob.MCRJob;
+import org.mycore.services.queuedjob.MCRJobAction;
+
+/**
+ * @author Ren\u00E9 Adler (eagle)
+ *
+ */
+public class MailJob extends MCRJobAction {
+
+    public MailJob() {
+    }
+
+    /**
+     * @param job
+     */
+    public MailJob(MCRJob job) {
+        super(job);
+    }
+
+    /* (non-Javadoc)
+     * @see org.mycore.services.queuedjob.MCRJobAction#isActivated()
+     */
+    @Override
+    public boolean isActivated() {
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see org.mycore.services.queuedjob.MCRJobAction#name()
+     */
+    @Override
+    public String name() {
+        return MailJob.class.getSimpleName();
+    }
+
+    /* (non-Javadoc)
+     * @see org.mycore.services.queuedjob.MCRJobAction#execute()
+     */
+    @Override
+    public void execute() throws ExecutionException {
+        final String uri = job.getParameter("uri");
+
+        try {
+            final Element xml = MCRURIResolver.instance().resolve(uri);
+            final String to = xml.getAttributeValue("to");
+
+            if (to != null && !to.isEmpty())
+                MCRMailer.send(xml);
+        } catch (Exception ex) {
+            throw new ExecutionException(ex);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.mycore.services.queuedjob.MCRJobAction#rollback()
+     */
+    @Override
+    public void rollback() {
+        // nothing to do
+    }
+
+}
---
+++ src/main/java/org/urmel/dbt/common/MailQueue.java
@@ -0,0 +1,51 @@
+/*
+ * $Id$ 
+ * $Revision$ $Date$
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+package org.urmel.dbt.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mycore.services.queuedjob.MCRJob;
+import org.mycore.services.queuedjob.MCRJobQueue;
+
+/**
+ * @author Ren\u00E9 Adler (eagle)
+ *
+ */
+public class MailQueue {
+
+    private static final MCRJobQueue MAIL_QUEUE = MCRJobQueue.getInstance(MailJob.class);
+
+    public static void addJob(final String uri) {
+        final Map<String, String> params = new HashMap<String, String>();
+        params.put("uri", uri);
+
+        MCRJob job = MAIL_QUEUE.getJob(params);
+        if (job == null) {
+            job = new MCRJob(MailJob.class);
+            job.setParameters(params);
+        }
+
+        MAIL_QUEUE.offer(job);
+    }
+}
