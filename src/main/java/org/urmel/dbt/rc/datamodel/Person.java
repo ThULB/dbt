13382d9d55151669a746cd2e394fac54a63080dc
René Adler <adlerre@users.noreply.github.com>
2015-06-30 07:03:28 +0000

    implement standard methods
    git-svn-id: https://svn.thulb.uni-jena.de/repos/dbt/trunk@2426 f4d8827f-d2b3-4e18-a982-0b0116c0c996

--- src/main/java/org/urmel/dbt/rc/datamodel/Person.java
+++ src/main/java/org/urmel/dbt/rc/datamodel/Person.java
@@ -86,4 +86,81 @@ public abstract class Person implements Serializable {
     public void setOrigin(final String origin) {
         this.origin = origin;
     }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Person [");
+        if (name != null) {
+            builder.append("name=");
+            builder.append(name);
+            builder.append(", ");
+        }
+        if (email != null) {
+            builder.append("email=");
+            builder.append(email);
+            builder.append(", ");
+        }
+        if (origin != null) {
+            builder.append("origin=");
+            builder.append(origin);
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((origin == null) ? 0 : origin.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Person)) {
+            return false;
+        }
+        Person other = (Person) obj;
+        if (email == null) {
+            if (other.email != null) {
+                return false;
+            }
+        } else if (!email.equals(other.email)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (origin == null) {
+            if (other.origin != null) {
+                return false;
+            }
+        } else if (!origin.equals(other.origin)) {
+            return false;
+        }
+        return true;
+    }
 }
