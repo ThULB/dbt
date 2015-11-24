db9776d3076c2766a59faf881f271a03600c3ce4
René Adler <adlerre@users.noreply.github.com>
2015-11-24 09:22:17 +0000

    implement rc slot export/import commands
    git-svn-id: https://svn.thulb.uni-jena.de/repos/dbt/trunk@2666 f4d8827f-d2b3-4e18-a982-0b0116c0c996

--- src/main/java/org/urmel/dbt/rc/commandline/RCCommands.java
+++ src/main/java/org/urmel/dbt/rc/commandline/RCCommands.java
@@ -22,13 +22,22 @@
  */
 package org.urmel.dbt.rc.commandline;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mycore.common.MCRPersistenceException;
+import org.mycore.common.content.MCRContent;
 import org.mycore.common.events.MCREvent;
 import org.mycore.common.events.MCREventManager;
 import org.mycore.datamodel.common.MCRActiveLinkException;
@@ -42,8 +51,12 @@ import org.urmel.dbt.rc.datamodel.Status;
 import org.urmel.dbt.rc.datamodel.Warning;
 import org.urmel.dbt.rc.datamodel.WarningDate;
 import org.urmel.dbt.rc.datamodel.slot.Slot;
+import org.urmel.dbt.rc.datamodel.slot.SlotEntry;
 import org.urmel.dbt.rc.datamodel.slot.SlotList;
+import org.urmel.dbt.rc.datamodel.slot.entries.FileEntry;
+import org.urmel.dbt.rc.persistency.FileEntryManager;
 import org.urmel.dbt.rc.persistency.SlotManager;
+import org.urmel.dbt.rc.utils.SlotTransformer;
 
 /**
  * @author Ren\u00E9 Adler (eagle)
@@ -53,6 +66,181 @@ import org.urmel.dbt.rc.persistency.SlotManager;
 public class RCCommands extends MCRAbstractCommands {
 
     private static final Logger LOGGER = LogManager.getLogger(RCCommands.class);
+
+    @SuppressWarnings("unchecked")
+    @MCRCommand(syntax = "export slot {0} to directory {1}", help = "exports rc slot with id {0} to given directory {1}")
+    public static void exportSlot(final String slotId, final String dirname) throws IOException {
+        final SlotManager mgr = SlotManager.instance();
+        final SlotList slotList = mgr.getSlotList();
+
+        final Slot slot = slotList.getSlotById(slotId);
+        if (slot != null) {
+            File dir = new File(dirname);
+            if (!dir.isDirectory()) {
+                LOGGER.error(dirname + " is not a dirctory.");
+                return;
+            }
+
+            File xmlOutput = new File(dir, "slot-" + slotId + ".xml");
+            SlotTransformer.sendTo(slot, xmlOutput);
+            LOGGER.info("Slot " + slotId + " saved to " + xmlOutput.getCanonicalPath() + ".");
+
+            if (slot.getEntries() != null) {
+                for (SlotEntry<?> entry : slot.getEntries()) {
+                    if (entry.getEntry() instanceof FileEntry) {
+                        SlotEntry<FileEntry> fileEntry = (SlotEntry<FileEntry>) entry;
+
+                        File fileDir = new File(dirname + File.separator + fileEntry.getId());
+                        if (fileDir.isDirectory() || fileDir.mkdirs()) {
+                            try {
+                                MCRContent c = FileEntryManager.retrieve(slot, fileEntry);
+                                File f = new File(fileDir, fileEntry.getEntry().getName());
+                                c.sendTo(f);
+                                LOGGER.info("File \"" + fileEntry.getEntry().getName() + "\" saved to "
+                                        + f.getCanonicalPath() + ".");
+                            } catch (Exception ex) {
+                                LOGGER.error(ex.getMessage());
+                                LOGGER.error("Exception while store file to " + fileDir.getAbsolutePath());
+                                return;
+                            }
+                        } else {
+                            LOGGER.error("Couldn't create a directory for file entry.");
+                            return;
+                        }
+                    }
+                }
+            }
+        } else {
+            LOGGER.error("Couldn't found a rc slot with id " + slotId);
+        }
+    }
+
+    @MCRCommand(syntax = "export all slots to directory {0}", help = "exports all rc slots to given directory {0}")
+    public static List<String> exportAllSlots(final String dirname) throws IOException {
+        final SlotManager mgr = SlotManager.instance();
+        final SlotList slotList = mgr.getSlotList();
+
+        File dir = new File(dirname);
+        if (!dir.isDirectory()) {
+            LOGGER.error(dirname + " is not a dirctory.");
+            return Collections.emptyList();
+        }
+
+        if (!slotList.getSlots().isEmpty()) {
+            List<String> cmds = new ArrayList<String>(slotList.getSlots().size());
+            for (final Slot slot : slotList.getSlots()) {
+                File slotDir = new File(dir, slot.getSlotId());
+                if (slotDir.isDirectory() || slotDir.mkdirs()) {
+                    String command = MessageFormat.format("export slot {0} to directory {1}", slot.getSlotId(),
+                            slotDir.getAbsolutePath());
+                    cmds.add(command);
+                }
+            }
+            return cmds;
+        }
+
+        return Collections.emptyList();
+    }
+
+    @SuppressWarnings("unchecked")
+    @MCRCommand(syntax = "import slot from file {0}", help = "imports a slot from given file")
+    public static void importSlot(final String filename)
+            throws IOException, MCRPersistenceException, MCRActiveLinkException {
+        final SlotManager mgr = SlotManager.instance();
+
+        File file = new File(filename);
+        if (!file.isFile()) {
+            LOGGER.error(filename + " is not a file.");
+            return;
+        }
+
+        if (!file.getName().endsWith(".xml")) {
+            LOGGER.error(file + " does not end with *.xml");
+            return;
+        }
+
+        final Slot slot = SlotTransformer.buildSlot(file.toURI());
+        final Slot oldSlot = mgr.getSlotById(slot.getSlotId());
+
+        boolean update = oldSlot != null;
+        if (update) {
+            slot.setMCRObjectID(oldSlot.getMCRObjectID());
+        }
+
+        if (slot.getEntries() != null) {
+            for (SlotEntry<?> entry : slot.getEntries()) {
+                if (entry.getEntry() instanceof FileEntry) {
+                    SlotEntry<FileEntry> fileEntry = (SlotEntry<FileEntry>) entry;
+
+                    File f = new File(file.getParent(),
+                            fileEntry.getId() + File.separator + fileEntry.getEntry().getName());
+                    if (f.isFile()) {
+                        InputStream is = null;
+                        try {
+                            is = new FileInputStream(f);
+                            fileEntry.getEntry().setContent(is);
+                        } catch (FileNotFoundException e) {
+                            LOGGER.error("Couldn't not read file \"" + f.getCanonicalPath() + "\" for file entry.");
+                            return;
+                        } finally {
+                            if (is != null)
+                                is.close();
+                        }
+                    } else {
+                        LOGGER.error("Couldn't find file for file entry.");
+                        return;
+                    }
+
+                    if (update) {
+                        LOGGER.info("Update File \"" + fileEntry.getEntry().getName() + "\" from "
+                                + f.getCanonicalPath() + ".");
+                        FileEntryManager.update(slot, fileEntry);
+                    }
+                }
+            }
+        }
+
+        if (update) {
+            LOGGER.info("Update Slot " + slot.getSlotId() + " from " + file.getCanonicalPath() + ".");
+            mgr.setSlot(slot);
+            mgr.saveOrUpdate(slot);
+        } else {
+            LOGGER.info("Import Slot " + slot.getSlotId() + " from " + file.getCanonicalPath() + ".");
+            mgr.addSlot(slot);
+            mgr.saveOrUpdate(slot);
+        }
+    }
+
+    @MCRCommand(syntax = "import all slots from directory {0}", help = "imports all rc slots from given directory {0}")
+    public static List<String> importAllSlots(final String dirname) throws IOException {
+        final File dir = new File(dirname);
+        if (!dir.isDirectory()) {
+            LOGGER.error(dirname + " is not a dirctory.");
+            return Collections.emptyList();
+        }
+
+        final String[] list = dir.list();
+
+        if (list.length == 0) {
+            LOGGER.warn("No files found in directory " + dirname);
+            return Collections.emptyList();
+        }
+
+        List<String> cmds = new ArrayList<String>();
+        for (final String r : list) {
+            final File fr = new File(dir, r);
+            if (fr.isDirectory()) {
+                for (final String c : fr.list()) {
+                    if (c.endsWith(".xml") && c.contains("slot")) {
+                        String command = MessageFormat.format("import slot from file {0}",
+                                new File(fr, c).getAbsolutePath());
+                        cmds.add(command);
+                    }
+                }
+            }
+        }
+        return cmds;
+    }
 
     @MCRCommand(syntax = "rc inactivator", help = "send warning mails for reserve collections or inactivate, set new status")
     public static void rcInactivator() throws IOException {
--- src/main/java/org/urmel/dbt/rc/utils/SlotTransformer.java
+++ src/main/java/org/urmel/dbt/rc/utils/SlotTransformer.java
@@ -3,7 +3,9 @@
  */
 package org.urmel.dbt.rc.utils;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -15,6 +17,8 @@ import org.jdom2.JDOMException;
 import org.jdom2.transform.JDOMSource;
 import org.mycore.common.MCRException;
 import org.mycore.common.content.MCRJAXBContent;
+import org.mycore.common.content.MCRVFSContent;
+import org.mycore.common.xml.MCRXMLParserFactory;
 import org.urmel.dbt.rc.datamodel.slot.Slot;
 import org.xml.sax.SAXParseException;
 
@@ -49,6 +53,15 @@ public abstract class SlotTransformer {
         return getDocument(slot);
     }
 
+    public static void sendTo(final Slot slot, final File target) {
+        final MCRJAXBContent<Slot> content = new MCRJAXBContent<Slot>(JAXB_CONTEXT, slot);
+        try {
+            content.sendTo(target);
+        } catch (final IOException e) {
+            throw new MCRException("Exception while transforming Slot to File.", e);
+        }
+    }
+
     public static Slot buildSlot(final Element element) {
         if (!element.getName().equals(ROOT_ELEMENT_NAME)) {
             throw new IllegalArgumentException("Element is not a rc slot element.");
@@ -58,6 +71,15 @@ public abstract class SlotTransformer {
             return (Slot) unmarshaller.unmarshal(new JDOMSource(element));
         } catch (final JAXBException e) {
             throw new MCRException("Exception while transforming Element to Slot.", e);
+        }
+    }
+
+    public static Slot buildSlot(final URI uri) {
+        try {
+            Document jdom = MCRXMLParserFactory.getParser(false).parseXML(new MCRVFSContent(uri));
+            return buildSlot(jdom.getRootElement());
+        } catch (MCRException | SAXParseException | IOException e) {
+            throw new MCRException("Exception while transforming URI to Slot.", e);
         }
     }
 }
