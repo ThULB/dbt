a7dce34cbdbe34c875694f52748f8329e9181a12
René Adler <adlerre@users.noreply.github.com>
2016-02-18 09:54:41 +0000

    try to get xsl stylesheet from jar
    git-svn-id: https://svn.thulb.uni-jena.de/repos/dbt/trunk@2748 f4d8827f-d2b3-4e18-a982-0b0116c0c996

--- src/main/java/org/urmel/dbt/migration/MigrationCommands.java
+++ src/main/java/org/urmel/dbt/migration/MigrationCommands.java
@@ -22,11 +22,14 @@
  */
 package org.urmel.dbt.migration;
 
+import java.io.File;
+import java.net.URL;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.mycore.datamodel.common.MCRXMLMetadataManager;
 import org.mycore.datamodel.ifs2.MCRMetadataStore;
 import org.mycore.datamodel.metadata.MCRObjectID;
@@ -41,15 +44,29 @@ import org.mycore.frontend.cli.annotation.MCRCommandGroup;
 @MCRCommandGroup(name = "Migration Commands")
 public class MigrationCommands extends MCRAbstractCommands {
 
+    private static final Logger LOGGER = Logger.getLogger(MigrationCommands.class);
+
     @MCRCommand(syntax = "fix objects for base {0} with file {1}", help = "transforms all mycore objects for base {0} with the given file or URL {1}")
     public static List<String> xsltObjects(final String base, final String xslFile) throws Exception {
+        URL styleFile = MigrationCommands.class.getResource("/xsl/" + xslFile);
+        if (styleFile == null) {
+            final File file = new File(xslFile);
+
+            if (!file.exists()) {
+                LOGGER.error("Could not find the stylesheet \"" + xslFile + "\".");
+                return null;
+            }
+
+            styleFile = file.toURI().toURL();
+        }
+
         List<String> cmds = new ArrayList<String>();
 
         MCRMetadataStore store = MCRXMLMetadataManager.instance().getStore(base);
         Iterator<Integer> IDs = store.listIDs(true);
         while (IDs.hasNext()) {
             final String id = MCRObjectID.formatID(base, IDs.next());
-            cmds.add(MessageFormat.format("xslt {0} with file {1}", id, xslFile));
+            cmds.add(MessageFormat.format("xslt {0} with file {1}", id, styleFile.toString()));
         }
 
         return cmds;
