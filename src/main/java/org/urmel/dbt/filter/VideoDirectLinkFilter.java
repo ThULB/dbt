6e975cababbf1eddb0bce59ff5dfced18ba6cacb
René Adler <adlerre@users.noreply.github.com>
2016-05-04 07:47:25 +0000

    DBT-10 #comment implement direct link filter
    git-svn-id: https://svn.thulb.uni-jena.de/repos/dbt/trunk@2819 f4d8827f-d2b3-4e18-a982-0b0116c0c996

---
+++ src/main/java/org/urmel/dbt/filter/VideoDirectLinkFilter.java
@@ -0,0 +1,137 @@
+/*
+ * $Id$ 
+ * $Revision$ $Date$
+ *
+ * This file is part of ***  M y C o R e  ***
+ * See http://www.mycore.de/ for details.
+ *
+ * This program is free software; you can use it, redistribute it
+ * and / or modify it under the terms of the GNU General Public License
+ * (GPL) as published by the Free Software Foundation; either version 2
+ * of the License or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, in a file called gpl.txt or license.txt.
+ * If not, write to the Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307 USA
+ */
+package org.urmel.dbt.filter;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mycore.datamodel.niofs.MCRPath;
+import org.mycore.frontend.MCRFrontendUtil;
+import org.mycore.frontend.filter.MCRSecureTokenV2FilterConfig;
+import org.mycore.frontend.support.MCRSecureTokenV2;
+
+/**
+ * @author Ren\u00E9 Adler (eagle)
+ *
+ */
+public class VideoDirectLinkFilter implements Filter {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private static final Pattern PATTERN_DERIVATE_ID = Pattern.compile(".+_derivate_[0-9]+");
+
+    private boolean filterEnabled = true;
+
+    private String hashParameter;
+
+    private String sharedSecret;
+
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        filterEnabled = MCRSecureTokenV2FilterConfig.isFilterEnabled();
+        hashParameter = MCRSecureTokenV2FilterConfig.getHashParameterName();
+        sharedSecret = MCRSecureTokenV2FilterConfig.getSharedSecret();
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (filterEnabled) {
+            final HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+            final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+            final String pathInfo = httpServletRequest.getPathInfo();
+
+            if (pathInfo != null && validateReferrer(httpServletRequest)
+                    && MCRSecureTokenV2FilterConfig.requireHash(pathInfo)) {
+                MCRSecureTokenV2 token = new MCRSecureTokenV2(httpServletRequest.getPathInfo().substring(1),
+                        MCRFrontendUtil.getRemoteAddr(httpServletRequest), sharedSecret);
+                try {
+                    httpServletResponse.sendRedirect(
+                            token.toURI(MCRFrontendUtil.getBaseURL() + "servlets/MCRFileNodeServlet/", hashParameter)
+                                    .toASCIIString());
+                    return;
+                } catch (URISyntaxException e) {
+                    throw new ServletException(e);
+                }
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    private boolean validateReferrer(final HttpServletRequest httpServletRequest) {
+        final String referrer = httpServletRequest.getHeader("referer");
+        if (referrer != null) {
+            try {
+                final String pathInfo = new URL(referrer).getPath();
+                if (pathInfo.endsWith(".html")) {
+                    LOGGER.info("pathInfo: " + pathInfo.substring(1));
+
+                    Optional<String> optDerId = Arrays.stream(pathInfo.split("/"))
+                            .filter(f -> PATTERN_DERIVATE_ID.matcher(f).matches())
+                            .findFirst();
+
+                    if (optDerId.isPresent()) {
+                        final String derivateId = optDerId.get();
+                        final String fileName = pathInfo.substring(pathInfo.indexOf(derivateId) + derivateId.length());
+                        return Files.exists(MCRPath.getPath(derivateId, fileName));
+                    }
+                }
+            } catch (MalformedURLException e) {
+                LOGGER.error("Couldn't parse referrer " + referrer + ".", e);
+            }
+        }
+
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#destroy()
+     */
+    @Override
+    public void destroy() {
+    }
+
+}
--- src/main/java/org/urmel/dbt/internal/DBTAutoDeploy.java
+++ src/main/java/org/urmel/dbt/internal/DBTAutoDeploy.java
@@ -26,20 +26,22 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
-import java.util.jar.JarEntry;
+import java.util.Optional;
 import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextListener;
-import javax.servlet.ServletRegistration;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.jdom2.JDOMException;
 import org.jdom2.Namespace;
 import org.jdom2.input.SAXBuilder;
 import org.mycore.common.events.MCRShutdownHandler;
@@ -79,104 +81,159 @@ public class DBTAutoDeploy implements MCRStartupHandler.AutoExecutable, MCRShutd
         return 2000;
     }
 
+    @SuppressWarnings("resource")
     @Override
     public void startUp(final ServletContext servletContext) {
         if (servletContext != null) {
-            registerServlets(servletContext);
+            registerWebFragment(servletContext);
 
             final String webRoot = servletContext.getRealPath("/");
             if (webRoot != null) {
                 final String jarFile = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-                JarFile jar;
                 try {
+                    final JarFile jar = new JarFile(jarFile);
+
                     LOGGER.info("Deploy DBT web resources to \"" + webRoot + "\"...");
+                    Collections.list(jar.entries()).stream().filter(file -> file.getName().startsWith(RESOURCE_DIR))
+                            .forEach(file -> {
+                                final String fileName = file.getName().substring(RESOURCE_DIR.length());
+                                LOGGER.debug("...deploy " + fileName);
 
-                    jar = new JarFile(jarFile);
-                    final Enumeration<JarEntry> enumEntries = jar.entries();
-                    while (enumEntries.hasMoreElements()) {
-                        final JarEntry file = (JarEntry) enumEntries.nextElement();
-                        if (file.getName().startsWith(RESOURCE_DIR)) {
-                            final String fileName = file.getName().substring(RESOURCE_DIR.length());
-                            LOGGER.debug("...deploy " + fileName);
+                                final File f = new File(webRoot + File.separator + fileName);
+                                if (file.isDirectory()) {
+                                    f.mkdir();
+                                    return;
+                                }
 
-                            final File f = new File(webRoot + File.separator + fileName);
-                            if (file.isDirectory()) {
-                                f.mkdir();
-                                continue;
-                            }
-
-                            final InputStream is = jar.getInputStream(file);
-                            final FileOutputStream fos = new FileOutputStream(f);
-                            while (is.available() > 0) {
-                                fos.write(is.read());
-                            }
-                            fos.close();
-                            is.close();
-                        }
-                    }
+                                try {
+                                    final InputStream is = jar.getInputStream(file);
+                                    final FileOutputStream fos = new FileOutputStream(f);
+                                    while (is.available() > 0) {
+                                        fos.write(is.read());
+                                    }
+                                    fos.close();
+                                } catch (IOException e) {
+                                    LOGGER.error("Couldn't deploy file " + fileName + ".", e);
+                                }
+                            });
                     LOGGER.info("...done.");
+
+                    jar.close();
                 } catch (final IOException e) {
-                    LOGGER.error("Couldn't parse JAR!");
+                    LOGGER.error("Couldn't parse JAR!", e);
                 }
             }
         }
     }
 
-    private void registerServlets(final ServletContext servletContext) {
+    private void registerWebFragment(final ServletContext servletContext) {
         final String jarFile = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-        JarFile jar;
         try {
-            Document doc = null;
-            jar = new JarFile(jarFile);
-            final Enumeration<JarEntry> enumEntries = jar.entries();
-            while (enumEntries.hasMoreElements()) {
-                final JarEntry file = (JarEntry) enumEntries.nextElement();
-                if (file.getName().equals(WEB_FRAGMENT)) {
-                    final SAXBuilder builder = new SAXBuilder();
+            final JarFile jar = new JarFile(jarFile);
 
-                    final InputStream is = jar.getInputStream(file);
-                    doc = builder.build(is);
-                    is.close();
+            Collections.list(jar.entries()).stream()
+                    .filter(file -> file.getName().equals(WEB_FRAGMENT))
+                    .findFirst().ifPresent(file -> {
+                        final SAXBuilder builder = new SAXBuilder();
+                        try {
+                            final InputStream is = jar.getInputStream(file);
+                            final Document doc = builder.build(is);
 
-                    break;
-                }
-            }
+                            final Element root = doc.getRootElement();
+                            final Namespace ns = root.getNamespace();
 
-            if (doc != null) {
-                final Element root = doc.getRootElement();
-                final Namespace ns = root.getNamespace();
-                final List<Element> servlets = root.getChildren("servlet", ns);
-                final List<Element> mappings = root.getChildren("servlet-mapping", ns);
+                            final List<Element> filters = root.getChildren("filter", ns);
+                            final List<Element> fmaps = root.getChildren("filter-mapping", ns);
 
-                for (Element servlet : servlets) {
-                    final String name = servlet.getChildText("servlet-name", ns);
-                    final String clazz = servlet.getChildText("servlet-class", ns);
+                            filters.forEach(filter -> {
+                                final String name = filter.getChildText("filter-name", ns);
+                                final String className = filter.getChildText("filter-class", ns);
 
-                    final List<String> urlPattern = new ArrayList<String>();
-                    for (Element mapping : mappings) {
-                        final String n = mapping.getChildText("servlet-name", ns);
-                        if (n.equals(name)) {
-                            final List<Element> ups = mapping.getChildren("url-pattern", ns);
-                            for (Element up : ups) {
-                                urlPattern.add(up.getTextTrim());
-                            }
-                            break;
+                                fmaps.stream().filter(mapping -> mapping.getChildText("filter-name", ns).equals(name))
+                                        .findFirst().ifPresent(mapping -> {
+                                            LOGGER.info("Register Filter " + name + " (" + className + ")...");
+                                            Optional.ofNullable(servletContext.addFilter(name, className))
+                                                    .<Runnable> map(fr -> () -> {
+                                                        final List<Element> dispatchers = mapping
+                                                                .getChildren("dispatcher", ns);
+
+                                                        final EnumSet<DispatcherType> eDT = dispatchers.isEmpty() ? null
+                                                                : dispatchers.stream()
+                                                                        .map(d -> DispatcherType
+                                                                                .valueOf(d.getTextTrim()))
+                                                                        .collect(Collectors.toCollection(
+                                                                                () -> EnumSet.noneOf(
+                                                                                        DispatcherType.class)));
+
+                                                        final List<Element> servletNames = mapping
+                                                                .getChildren("servlet-name", ns);
+
+                                                        if (!servletNames.isEmpty()) {
+                                                            fr.addMappingForServletNames(
+                                                                    eDT,
+                                                                    false,
+                                                                    servletNames.stream()
+                                                                            .map(sn -> {
+                                                                                LOGGER.info("...add servlet mapping: "
+                                                                                        + sn.getTextTrim());
+                                                                                return sn.getTextTrim();
+                                                                            })
+                                                                            .toArray(String[]::new));
+                                                        }
+
+                                                        final List<Element> urlPattern = mapping
+                                                                .getChildren("url-pattern", ns);
+
+                                                        if (!urlPattern.isEmpty()) {
+                                                            fr.addMappingForUrlPatterns(eDT,
+                                                                    false, urlPattern.stream()
+                                                                            .map(url -> {
+                                                                                LOGGER.info(
+                                                                                        "...add url mapping: "
+                                                                                                + url.getTextTrim());
+                                                                                return url.getTextTrim();
+                                                                            })
+                                                                            .toArray(String[]::new));
+                                                        }
+                                                    }).orElse(() -> LOGGER
+                                                            .warn("Filter " + name + " already registered!"))
+                                                    .run();
+                                        });
+                            });
+
+                            final List<Element> servlets = root.getChildren("servlet", ns);
+                            final List<Element> smaps = root.getChildren("servlet-mapping", ns);
+
+                            servlets.forEach(servlet -> {
+                                final String name = servlet.getChildText("servlet-name", ns);
+                                final String className = servlet.getChildText("servlet-class", ns);
+
+                                smaps.stream()
+                                        .filter(mapping -> mapping.getChildText("servlet-name", ns).equals(name))
+                                        .findFirst()
+                                        .ifPresent(mapping -> {
+                                            LOGGER.info("Register Servlet " + name + " (" + className + ")...");
+                                            Optional.ofNullable(servletContext.addServlet(name, className))
+                                                    .<Runnable> map(sr -> () -> {
+                                                        mapping.getChildren("url-pattern", ns).stream()
+                                                                .forEach(url -> {
+                                                                    LOGGER.info(
+                                                                            "...add url mapping: " + url.getTextTrim());
+                                                                    sr.addMapping(url.getTextTrim());
+                                                                });
+                                                    }).orElse(() -> LOGGER
+                                                            .error("Servlet" + name + " already registered!"))
+                                                    .run();
+                                        });
+                            });
+                        } catch (IOException | JDOMException e) {
+                            LOGGER.error("Couldn't parse " + WEB_FRAGMENT, e);
                         }
-                    }
+                    });
 
-                    LOGGER.info("Register Servlet " + name + " (" + clazz + ")...");
-                    final ServletRegistration sr = servletContext.addServlet(name, clazz);
-                    if (sr != null) {
-                        for (String up : urlPattern) {
-                            sr.addMapping(up);
-                        }
-                    } else {
-                        LOGGER.error("Couldn't register " + name + "!");
-                    }
-                }
-            }
-        } catch (final Exception e) {
-            e.printStackTrace();
+            jar.close();
+        } catch (final IOException e) {
+            LOGGER.error("Couldn't parse JAR!", e);
         }
     }
 }
