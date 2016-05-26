3bba46fa1e8c9805db70fae719fa3601feb367fa
René Adler <adlerre@users.noreply.github.com>
2016-05-26 08:35:49 +0000

    fix IE javascript error
    git-svn-id: https://svn.thulb.uni-jena.de/repos/dbt/trunk@2852 f4d8827f-d2b3-4e18-a982-0b0116c0c996

--- src/main/resources/META-INF/resources/dbt/js/migrationAlert.js
+++ src/main/resources/META-INF/resources/dbt/js/migrationAlert.js
@@ -1,24 +1,26 @@
-$(document).ready(function() {
-    var url = window.location.href;
-    var match = url.match(/\w+_mods_[0-9]+/g);
-    if (match != null && match[0] != undefined){
-    	match = match [0];
-		var idNumber = parseInt(match.replace(/\D/g, ""));
-		var migrationId = $("#migrationId").attr("data-migration-id");
-		var migrationUrl = $("#migrationUrl").attr("data-migration-url");
-		if (migrationId != undefined && migrationUrl != undefined) {
-			var migrationNumber = parseInt(migrationId.replace(/\D/g, ""));
-    		if (idNumber <= migrationNumber) {
-    			$("body").append(`
-	      			<div class="alert alert-warning alert-dismissible text-center" role="alert" style="position: fixed; bottom: 0; left: 0; width: 100%; margin-bottom: 0; border-radius: 0; z-index: 1000;">
-	      				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	      				Dieses Dokument wurde migriert. Falls Sie Probleme mit der Anzeige des Dokumentes haben, können Sie das Dokument auf dem <a href="`
-	      				+ migrationUrl
-	      				+ idNumber
-	      				+ `" class="alert-link" target="_blank">alten DBT Server</a> ansehen.
-	      			</div>
-    			`);
-    		}
-		}
-    }
-});
+$(document)
+		.ready(
+				function() {
+					var url = window.location.href;
+					var match = url.match(/\w+_mods_[0-9]+/g);
+					if (match != null && match[0] != undefined) {
+						match = match[0];
+						var idNumber = parseInt(match.replace(/\D/g, ""));
+						var migrationId = $("#migrationId").attr("data-migration-id");
+						var migrationUrl = $("#migrationUrl").attr("data-migration-url");
+						if (migrationId != undefined && migrationUrl != undefined) {
+							var migrationNumber = parseInt(migrationId.replace(/\D/g, ""));
+							if (idNumber <= migrationNumber) {
+								$("body")
+										.append(
+												"<div class=\"alert alert-warning alert-dismissible text-center\" role=\"alert\" style=\"position: fixed; bottom: 0; left: 0; width: 100%; margin-bottom: 0; border-radius: 0; z-index: 1000;\">"
+														+ "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">"
+														+ "<span aria-hidden=\"true\">&times;</span></button>"
+														+ "Dieses Dokument wurde migriert. Falls Sie Probleme mit der Anzeige des Dokumentes haben, können Sie das Dokument auf dem <a href=\""
+														+ migrationUrl
+														+ idNumber
+														+ "\" class=\"alert-link\" target=\"_blank\">alten DBT Server</a> ansehen.</div>");
+							}
+						}
+					}
+				});
