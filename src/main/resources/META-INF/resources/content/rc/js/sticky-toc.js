b926f468c0ee15dd2320ed2ce374dedb496836e9
René Adler <rene.adler@uni-jena.de>
2019-09-26 15:01:44 +0200

    remove console output

--- src/main/resources/META-INF/resources/content/rc/js/sticky-toc.js
+++ src/main/resources/META-INF/resources/content/rc/js/sticky-toc.js
@@ -5,7 +5,6 @@ var makeTocSticky = function () {
     var tocOffset = container.offsetTop + 10;
 
     if ($elm[0].offsetHeight < (window.innerHeight - tocOffset)) {
-      console.log($elm[0].offsetHeight, (window.innerHeight - tocOffset));
       $elm.addClass("sticky-top");
       $elm.css("top", tocOffset + "px");
     } else {
