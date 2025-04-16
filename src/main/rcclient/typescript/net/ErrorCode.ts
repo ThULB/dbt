7a62e36b472c4b4318d34a2148da9f86f8f8d6d3
René Adler <rene.adler@uni-jena.de>
2025-04-16 09:57:45 +0200

    drop rcclient

--- src/main/rcclient/typescript/net/ErrorCode.ts
+++
@@ -1,9 +0,0 @@
-module net {
-    export class ErrorCode {
-        public static UNKNOWN: number = 1;
-        public static NSRESULT: number = 1 << 1;
-        public static NOT_SUPPORTED: number = 1 << 2;
-        public static OFFLINE: number = 1 << 3;
-        public static HTTP_ERROR: number = 1 << 4;
-    }
-}
