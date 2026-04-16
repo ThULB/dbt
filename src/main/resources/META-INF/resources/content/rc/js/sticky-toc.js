9852b76fee4d52411d71254a19bd70fd705f9f59
René Adler <rene.adler@uni-jena.de>
2026-04-16 09:55:47 +0200

    DBT-406 fix sticky TOC

--- src/main/resources/META-INF/resources/content/rc/js/sticky-toc.js
+++ src/main/resources/META-INF/resources/content/rc/js/sticky-toc.js
@@ -1,26 +1,28 @@
-var makeTocSticky = function () {
-  var $elm = $(".slot-toc");
+const makeTocSticky = function () {
+    const $elm = $(".slot-toc");
 
-  if ($elm && window.innerWidth >= 1200) {
-    var container = document.getElementById("container-main");
-    var tocOffset = container.offsetTop + 10;
+    if ($elm && window.innerWidth >= 1200) {
+        const container = document.getElementById("main_content");
+        if (container) {
+            const tocOffset = container.offsetTop + 10;
 
-    if ($elm.height() < (window.innerHeight - tocOffset)) {
-      $elm.addClass("sticky-top");
-      $elm.css("top", tocOffset + "px");
-    } else {
-      $elm.removeClass("sticky-top");
-      $elm.css("top");
+            if ($elm.height() < (window.innerHeight - tocOffset)) {
+                $elm.addClass("sticky-top");
+                $elm.css("top", tocOffset + "px");
+            } else {
+                $elm.removeClass("sticky-top");
+                $elm.css("top");
+            }
+        }
     }
-  }
 };
 
 /**
  * The Main on DOM ready.
  */
 $(document).ready(function () {
-  makeTocSticky();
+    makeTocSticky();
 
-  window.addEventListener("orientationchange", makeTocSticky());
-  window.addEventListener("resize", makeTocSticky());
+    window.addEventListener("orientationchange", makeTocSticky());
+    window.addEventListener("resize", makeTocSticky());
 });
