f779c8e5678dba0deaeadf06b33f2155858460d9
René Adler <rene.adler@uni-jena.de>
2026-05-15 08:04:58 +0200

    fix possible NPE

--- src/main/java/de/urmel_dl/dbt/utils/DisplayNameConverter.java
+++ src/main/java/de/urmel_dl/dbt/utils/DisplayNameConverter.java
@@ -3,24 +3,25 @@
  * Copyright (c) 2000 - 2016
  * See <https://www.db-thueringen.de/> and <https://github.com/ThULB/dbt/>
  *
- * This program is free software: you can redistribute it and/or modify it under the 
+ * This program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this
  * program. If not, see <http://www.gnu.org/licenses/>.
  */
 package de.urmel_dl.dbt.utils;
 
+import org.mycore.user2.MCRUserAttributeConverter;
+
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-
-import org.mycore.user2.MCRUserAttributeConverter;
+import java.util.Optional;
 
 /**
  * @author René Adler (eagle)
@@ -30,7 +31,9 @@ public class DisplayNameConverter implements MCRUserAttributeConverter<String, S
 
     @Override
     public String convert(String value, String separator, Map<String, String> valueMapping) {
-        return new String(value.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+        return Optional.ofNullable(value)
+                .map(v -> new String(v.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8))
+                .orElse(null);
     }
 
 }
