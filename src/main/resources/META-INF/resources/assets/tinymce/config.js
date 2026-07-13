eebcfcc6e218beeccf605da87d7ff0bbcf7c727e
Thomas Scheffler <github@yagee.de>
2026-07-13 14:05:26 +0200

    DBT-417 allow HTML links in abstracts

---
+++ src/main/resources/META-INF/resources/assets/tinymce/config.js
@@ -0,0 +1,22 @@
+
+tinymce.init({
+    selector: "textarea.tinymce",
+    license_key: "gpl",
+    promotion: false,
+    language: currentLang,
+    plugins: [
+        "advlist", "anchor", "autolink", "code", "fullscreen", "help",
+        "link", "lists", "preview",
+        "searchreplace", "table", "visualblocks", "wordcount"
+    ],
+    menubar: false,
+    toolbar: "undo redo | styles | bold italic underline | link | remove | code | indent outdent |" +
+        " alignleft aligncenter alignright alignjustify | bullist numlist | table | hr",
+    toolbar_mode: "wrap",
+    entity_encoding: "raw",
+    valid_elements: window["MIR.WebConfig.Editor.TinyMCE.ValidElements"] || "",
+    convert_urls: false,
+    verify_html: true,
+    link_target_list: false,
+    link_title: false
+});
